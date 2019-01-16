fc9577a2238519a0cbe2783d5f249d9d80f1e97b
yingyinggao <yingyinggao@sohu-inc.com>
2019-01-16 18:58:04 +0800

    随机获取颜色值

--- index.js
+++ index.js
@@ -259,6 +259,17 @@ export const cnLen = (str) => {
   return len;
 }
 
+// 随机获取一个颜色值
+export getRandomColor = () => {
+  const rgb = []
+  for (let i = 0 ; i < 3; ++i){
+    let color = Math.floor(Math.random() * 256).toString(16)
+    color = color.length == 1 ? '0' + color : color
+    rgb.push(color)
+  }
+  return '#' + rgb.join('')
+}
+
 
 
 
