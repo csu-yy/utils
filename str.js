17c8209fd82d52a211b2c8103331bc879dd265d9
skyeGao <yyjzp1314@126.com>
2019-07-07 15:25:04 +0800

    正则表达式解析URL

--- str.js
+++ str.js
@@ -76,3 +76,19 @@ export const elementIsInViewport = (el, offset = 0) => {
   return (top && left && bottom && right);
 }
 
+// 正则表达式解析URL
+export const getQueryObject = (url) => {
+  url = url == null ? window.location.href : url;
+  var search = url.substring(url.lastIndexOf("?") + 1);
+  var obj = {};
+  var reg = /([^?&=]+)=([^?&=]*)/g;
+  search.replace(reg, function (rs, $1, $2) {
+      var name = decodeURIComponent($1);
+      var val = decodeURIComponent($2);                
+      val = String(val);
+      obj[name] = val;
+      return rs;
+  });
+  return obj;
+}
+
